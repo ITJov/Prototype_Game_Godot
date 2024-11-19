bbd1dcb41045afa1ba2262bcf79da07805bc72c3
Joshua Subianto <81760236+JGkwen@users.noreply.github.com>
2024-11-19 14:47:24 +0700

    hmm
    test kesekian

--- script/Main.gd
+++ script/Main.gd
@@ -1,8 +1,39 @@
 extends Node2D
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta) -> void:
+	if(Input.is_action_pressed("ui_right")):
+		$Player_Orang.move_and_collide(Vector2(10,0))
+		print("kanan")
+	elif(Input.is_action_pressed("ui_left")):
+		$Player_Orang.move_and_collide(Vector2(-10,0))
+		print("kiri")
+	elif(Input.is_action_pressed("ui_up")):
+		$Player_Orang.move_and_collide(Vector2(0,-10))
+		print("atas")
+	elif(Input.is_action_pressed("ui_down")):
+		$Player_Orang.move_and_collide(Vector2(0,10))
+		print("bawah")
 	pass
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if(Input.is_action_pressed("ui_right")):
+		#$Player.position = $Player.position + Vector2(10,0)
+		#print("kanan")
+	#elif(Input.is_action_pressed("ui_left")):
+		#$Player.position = $Player.position + Vector2(-10,0)
+		#print("kiri")
+	#elif(Input.is_action_pressed("ui_up")):
+		#$Player.position = $Player.position + Vector2(0,-10)
+		#print("atas")
+	#elif(Input.is_action_pressed("ui_down")):
+		#$Player.position = $Player.position + Vector2(0,10)
+		#print("bawah")
+	#pass
