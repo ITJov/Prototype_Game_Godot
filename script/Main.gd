a49f01b58c0361a4e06e8ebebf8fb3652ae47585
Josephine Alvina L <josephinealvinaluwia@gmail.com>
2024-12-10 15:03:22 +0700

    animasi jalan

--- script/Main.gd
+++ script/Main.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var current_dir = 'none'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,18 +9,55 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta) -> void:
 	if(Input.is_action_pressed("ui_right")):
-		$Player_Orang.move_and_collide(Vector2(10,0))
+		current_dir = 'right'
+		play_animation(1)
+		$Player_Orang.move_and_collide(Vector2(1,0))
 		#print("kanan")
 	elif(Input.is_action_pressed("ui_left")):
-		$Player_Orang.move_and_collide(Vector2(-10,0))
+		current_dir = 'left'
+		play_animation(1)
+		$Player_Orang.move_and_collide(Vector2(-1,0))
 		#print("kiri")
 	elif(Input.is_action_pressed("ui_up")):
-		$Player_Orang.move_and_collide(Vector2(0,-10))
+		current_dir = 'up'
+		play_animation(1)
+		$Player_Orang.move_and_collide(Vector2(0,-1))
 		#print("atas")
 	elif(Input.is_action_pressed("ui_down")):
-		$Player_Orang.move_and_collide(Vector2(0,10))
+		current_dir = 'down'
+		play_animation(1)
+		$Player_Orang.move_and_collide(Vector2(0,1))
 		#print("bawah")
+	else:
+		play_animation(0)
 	pass
+
+func play_animation(movement):
+	var dir = current_dir
+	var animation = $Player_Orang/Karakter
+	
+	match movement:
+		1:
+			match dir :
+				'right':
+					animation.play('right')
+				'left':
+					animation.play('left')
+				'up':
+					animation.play('backward')
+				'down':
+					animation.play('forward')
+		0:
+			match dir :
+				'right':
+					animation.play('idle_right')
+				'left':
+					animation.play('idle_left')
+				'up':
+					animation.play('idle_backward')
+				'down':
+					animation.play('idle_forward')
+			
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
